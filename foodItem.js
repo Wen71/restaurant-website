9c109cf9cbb38b8200ac54ebefaef062c09d1c51
Wen71 <wen.sun@mnsu.edu>
2023-01-03 20:49:20 -0600

    first commit

---
+++ foodItem.js
@@ -0,0 +1,123 @@
+const foodItem= [
+    {
+    id: 1,
+    name: 'Chicken Wings',
+    category : 'appitezer',
+    price: 6,
+    img: "images/appitezers/chicken wings.jpg",
+    quantity: 1
+},
+{
+    id: 2,
+    name: 'juicy bun',
+    category : 'appitezer',
+    price: 5,
+    img: 'images/appitezers/juicy bun.jpg',
+    quantity: 1
+},
+{
+    id: 3,
+    name: 'Pork Dumplings',
+    category : 'appitezer',
+    price: 8,
+    img: 'images/appitezers/pork dumplings.jpg',
+    quantity: 1
+},
+{
+    id: 4,
+    name: 'Scallion Pancakes',
+    category : 'appitezer',
+    price: 4,
+    img: 'images/appitezers/scallion pancakes.jpg',
+    quantity: 1
+},
+{
+    id: 5,
+    name: 'Spring rolls/春卷',
+    category : 'appitezer',
+    price: 4,
+    img: 'images/appitezers/spring rolls.jpg',
+    quantity: 1
+},
+{
+    id: 1,
+    name: 'Pineapple chicken',
+    category : 'american',
+    price: 9,
+    img: "images/american/Pineapple chicken.jpg",
+    quantity: 1
+},
+{
+    id: 2,
+    name: 'Sweet&sour chicken',
+    category : 'american',
+    price: 8,
+    img: "images/american/Sweet and sour chicken.jpg",
+    quantity: 1
+},
+{
+    id: 1,
+    name: '东坡肉',
+    category : 'chinese',
+    price: 14,
+    img: "images/chinese/Dong po pork.jpg",
+    quantity: 1
+},
+{
+    id: 2,
+    name: '麻婆豆腐',
+    category : 'chinese',
+    price: 10,
+    img: "images/chinese/Mapo Tofu.jpg",
+    quantity: 1
+},
+{
+    id: 3,
+    name: '鱼香肉丝',
+    category : 'chinese',
+    price: 13,
+    img: "images/chinese/Pork in Garlic Sauce.jpg",
+    quantity: 1
+},
+{
+    id: 4,
+    name: '海鲜豆腐煲',
+    category : 'chinese',
+    price: 12,
+    img: "images/chinese/Seafood tofu.jpg",
+    quantity: 1
+},
+{
+    id: 5,
+    name: '香辣牛肉',
+    category : 'chinese',
+    price: 15,
+    img: "images/chinese/Spicy SzeChuan Beef.jpg",
+    quantity: 1
+},
+{
+    id: 1,
+    name: 'hot&sour soup',
+    category : 'soup',
+    price: 7,
+    img: "images/soups/hot sour soup.jpg",
+    quantity: 1
+},
+{
+    id: 2,
+    name: 'Wonton soup',
+    category : 'soup',
+    price: 9,
+    img: "images/soups/shanghai wonton soup.jpg",
+    quantity: 1
+},
+{
+    id: 1,
+    name: 'tomato egg soup',
+    category : 'soup',
+    price: 7,
+    img: "images/soups/tomato egg soup.jpg",
+    quantity: 1
+},
+]
+export {foodItem};
